3bce99bc0fc24a04f02ab22fb768fea7c8e7f9be
Robot-Inventor <30.keita@gmail.com>
2024-11-08 01:19:01 +0900

    chore: fix ESLint errors

--- src/ts/loader.ts
+++ src/ts/loader.ts
@@ -1,6 +1,6 @@
-import browser from "webextension-polyfill";
+import { runtime } from "webextension-polyfill";
 
-const url = browser.runtime.getURL("js/main.js");
+const url = runtime.getURL("js/main.js");
 const script = document.createElement("script");
 script.src = url;
 script.addEventListener("load", () => {
